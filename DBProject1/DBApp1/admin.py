edf6bdb278cf867714240f5728423fbbfd1233f2
akkri1 <108869149+akkri1@users.noreply.github.com>
2022-07-12 06:25:43 -0400

    Delete DBProject1 directory

--- DBProject1/DBApp1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from DBApp1.models import Student
-class StudentAdmin(admin.ModelAdmin):
-    l = ['number','name','marks']
-admin.site.register(Student,StudentAdmin)
--- DBProject1/DBApp1/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    number = models.IntegerField()
-    name = models.CharField(max_length = 30)
-    marks = models.FloatField()
-    def __str__(self):
-        return self.name
