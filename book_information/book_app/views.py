8f66702c26d4f6027a2a225e786d8d958068208b
akkri1 <108869149+akkri1@users.noreply.github.com>
2022-07-12 06:26:29 -0400

    Delete book_information directory

--- book_information/book_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from book_app.models import Book
-# Create your views here.
-def book_list(request):
-    bk = Book.objects.all()
-    dic = {'book':bk}
-    return render(request,'book_app/index.html',dic)
