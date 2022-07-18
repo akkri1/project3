a05981a2565dc8f6abd91cf57515b03027a9d8c9
akkri1 <108869149+akkri1@users.noreply.github.com>
2022-07-18 00:55:35 -0400

    Update Web_scrapping_p.py

--- Web_scrapping_p.py
+++ Web_scrapping_p.py
@@ -43,7 +43,7 @@ def scraping(webpage):
         address.append(Address.text)
     
     #Generating the next page url
-    '''if page_number < 16:
+    '''if page_number < 15:
         
         page_number = page_number + 1
         scraping(webpage, page_number)'''
